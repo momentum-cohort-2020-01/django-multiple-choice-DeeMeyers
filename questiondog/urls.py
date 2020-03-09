eb1edd8279a79523fb9a31643961eeac17664667
DeeMeyers <justdmeyers@gmail.com>
2020-03-09 15:25:56 -0400

    basic homepage created

--- questiondog/urls.py
+++ questiondog/urls.py
@@ -16,9 +16,11 @@ Including another URLconf
 from django.contrib import admin
 from django.conf import settings
 from django.urls import include, path
+from users import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', views.homepage, name='homepage'),
 ]
 
 if settings.DEBUG:
