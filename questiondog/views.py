a70578760cf3e82c2a580514221b9885c1d1a259
DeeMeyers <justdmeyers@gmail.com>
2020-03-14 21:23:21 -0400

    progress was made i presume

--- questiondog/views.py
+++ questiondog/views.py
@@ -4,6 +4,8 @@ from django.contrib.auth.forms import UserCreationForm
 from django.conf import settings
 from django.contrib.auth.models import User
 from .models import DogPost, Comment
+from django.contrib.auth.decorators import login_required
+from django.views.decorators.http import require_GET, require_POST
 
 # Create your views here.
 
@@ -15,19 +17,24 @@ def homepage(request):
     else:
         return redirect('accounts/login/')
 
+
+@login_required
 def profile(request):
     posts = DogPost.objects.filter(user = request.user)
     comments = Comment.objects.filter(commentUser = request.user)
     return render(request, 'core/profile.html', {'posts': posts, 'comments': comments, } )
 
+@login_required
 def postdetail(request, pk):
     dogpost = DogPost.objects.get(pk=pk)
     coments = Comment.objects.filter(dogpost_id=pk).order_by('-created_at')
     return render(request, 'core/dogpostdetail.html', {'dogpost': dogpost, 'comments': coments, })
 
+@login_required
 def createPost(request):
     pass
 
+@login_required
 def createComment(request):
     pass
 
